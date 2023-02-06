03d0e7172e061d37478084749711c372eee09772
Maria Claudia Rincon Remolina <102395451+mrinconr11@users.noreply.github.com>
2023-02-06 15:40:21 -0500

    Update Lake_Generate_EEMs.py

--- Lake_Generate_EEMs.py
+++ Lake_Generate_EEMs.py
@@ -5,11 +5,8 @@ import pandas as pd
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 from functions import preprocess, preprocess_test, mae, r2, peak_pick, tucker
-import time
-decoder_shape = 7
-train_quantity = 144
-test_quantity = 36
 
+## Import training and testing datasets
 data = pd.read_csv(r'Lake_Train_cut.csv')
 df = pd.DataFrame(data)
 train_data = np.array(df)
@@ -18,14 +15,38 @@ test = pd.read_csv(r'Lake_Test_cut.csv')
 dft = pd.DataFrame(test)
 test_data = np.array(dft)
 input_data_test = np.nan_to_num(test_data)
-##
+
+## Normalize train and test dataset
+def preprocess(x):
+    y = np.copy(x)
+    max_v = np.max(x, axis=0)
+    min_v = np.min(x, axis=0)
+    for i in range(np.size(max_v)):
+        if (max_v[i] - min_v[i] > 0):
+            y[:, i] = (x[:, i] - min_v[i]) / (max_v[i] - min_v[i])
+        else:
+            continue
+    return y, max_v, min_v  # max and min vectors are needed to renormalize
+
+def preprocess_test(x, max_v, min_v):
+    y = np.copy(x)
+    for i in range(np.size(max_v)):
+        if (max_v[i] - min_v[i] > 0):
+            y[:, i] = (x[:, i] - min_v[i]) / (max_v[i] - min_v[i])
+        else:
+            continue
+    return y
+
 input_data_train, max_v, min_v = preprocess(input_data_train)
 input_data_test = preprocess_test(input_data_test, max_v, min_v)
 
+# Indexes of the key intensities selected
+indexes = [892, 1679, 1819]
+
+
+## Select few key intensities from the full EEM. The selected intensities correspond to the peak intensities of the contaminants and natural organic matter.
 test_decoder = np.zeros((test_quantity, decoder_shape))
 train_decoder = np.zeros((train_quantity, decoder_shape))
-
-indexes = [892, 1679, 1819, 3737, 2340, 4141, 4179]
 
 for i in range(decoder_shape):
     column_test = input_data_test[:, indexes[i]]
@@ -35,7 +56,7 @@ for i in range(decoder_shape):
     test_decoder[:, i] = column_test
     train_decoder[:, i] = column_train
 
-# set the input tensor
+# set the input tensor to generate the full EEM
 inputs_decoder = layers.Input(shape=(np.shape(train_decoder)[1],))
 def decoder(input):
     layer_1 = layers.Dense(units=20)(input)
@@ -52,13 +73,16 @@ def decoder(input):
     layer_4 = layers.Activation('elu')(layer_4)
     output = layers.Dense(units=4853, activation='elu')(layer_4)
     return output
-# get the decoder output
+
+# get the model output 
 deep_decoder_output = decoder(inputs_decoder)
 
 deep_decoder_model = Model(inputs=inputs_decoder, outputs=deep_decoder_output)
 deep_decoder_model.compile(optimizer='adam', loss='mean_squared_error')
 deep_decoder_model.summary()
 
+
+## Train the model
 deep_decoder_model.fit(
     x=train_decoder,
     y=input_data_train,
@@ -67,43 +91,53 @@ deep_decoder_model.fit(
     shuffle=True,
     validation_split=0.2
 )
+
 # Generate the EEMs
 decoder_predicted_test = deep_decoder_model.predict(test_decoder)
 decoder_predicted_train = deep_decoder_model.predict(train_decoder)
-##
+
 # Just an example plot to see how well it worked.
 plt.title('7 inputs')
 plt.plot(input_data_test[10,300:1000], '--', color='orange')
 plt.plot(decoder_predicted_test[10, 300:1000], '-')
 plt.show()
-##
-# Evaluate the results
+
+## Metrics to evaluate the reuslts. Mean absolute error (MAE), R2, Tucker coefficient.
+def r2(real, predicted):
+    mean = np.mean(real, axis=0)
+    first_errors_autoencoder = np.sum((real-predicted)**2, axis=0)
+    second_errors_autoencoder = np.sum((real-mean)**2, axis=0)
+    r2_autoencoder = 1-first_errors_autoencoder/second_errors_autoencoder
+    r2_autoencoder[r2_autoencoder == -inf] = nan
+    r2 = np.nanmean(r2_autoencoder)
+    return r2
+
+def tucker(real, predicted):
+    tucker_numerador_autoencoder = np.sum(np.multiply(real, predicted), axis=0)
+    tucker_demoninator_autoencoder = np.sqrt(np.sum((real)**2, axis=0) * np.sum((predicted)**2, axis=0))
+    tucker_autoencoder = tucker_numerador_autoencoder/tucker_demoninator_autoencoder
+    tucker = np.nanmean(tucker_autoencoder)
+    return tucker
+
+def mae(real, predicted):
+    mae = np.sum(abs(real-predicted), axis=0)
+    mae= mae/len(real)
+    mae = np.nanmean(mae)
+    return mae
+
 mae_decoder = float("{:.3f}".format(mae(input_data_test, decoder_predicted_test)))
 r2_decoder = float("{:.3f}".format(r2(input_data_test, decoder_predicted_test)))
 tucker_decoder = float("{:.3f}".format(tucker(input_data_test, decoder_predicted_test)))
-squared_error = np.mean(np.sqrt((input_data_test - decoder_predicted_test)**2))
-##
-# Save generated EEMS
-test_eem = pd.DataFrame(decoder_predicted_test)
-train_eem = pd.DataFrame(decoder_predicted_train)
-test_eem.to_csv('Lake_Test_Predicted_7.csv', index=False)
-train_eem.to_csv('Lake_Train_Predicted_7.csv', index=False)
-y = 0
 
-
-
-
-
-
-
-##
-train_concentration = pd.read_csv(r'../CNN/Lake_Train_Concentrations.csv')
+## Import concentrations. The generated EMMs will be used to predict contaminants concentrations.
+train_concentration = pd.read_csv(r'Lake_Train_Concentrations.csv')
 dfc = pd.DataFrame(train_concentration)
 train_concentration = np.array(dfc)
-test_concentration = pd.read_csv(r'../CNN/Lake_Test_Concentrations.csv')
+test_concentration = pd.read_csv(r'Lake_Test_Concentrations.csv')
 dfct = pd.DataFrame(test_concentration)
 test_concentration = np.array(dfct)
 
+## Separate phenol and naphthenic acids concentrations
 train_Naph = train_concentration[:,0]
 train_Phenol = train_concentration[:,1]
 train_Naph.shape = [train_quantity, 1]
@@ -112,99 +146,54 @@ test_Naph = test_concentration[:,0]
 test_Phenol = test_concentration[:,1]
 test_Naph.shape = [test_quantity, 1]
 test_Phenol.shape = [test_quantity, 1]
-##
+
+## ANN to predict contaminants concentrations from the generated EMMs.
 def naph(input):
     layer_1 = layers.Dense(units=1000, activation='elu')(input)
-    layer_2 = layers.Dense(units=500, activation='elu')(layer_1)
-    layer_21 = layers.Dense(units=100, activation='elu')(layer_2)
-    layer_3 = layers.Dense(units=50, activation='elu')(layer_21)
-    layer_4 = layers.Dense(units=10, activation='elu')(layer_3)
-    output = layers.Dense(units=1, activation='elu')(layer_4)
+    output = layers.Dense(units=1, activation='elu')(layer_1)
     return output
+
 inputs = layers.Input(shape=(np.shape(decoder_predicted_train)[1],))
 naph_output = naph(inputs)
 naph_model = Model(inputs=inputs, outputs=naph_output)
 naph_model.compile(optimizer='adam', loss='mean_squared_error')
+
+## Train the model
 deep_auto_history = naph_model.fit(
     x=decoder_predicted_train,
     y=train_Naph,
-    epochs=300,
+    epochs=500,
     batch_size=32,
     shuffle=True,
     validation_split=0.2
 )
 
+## Make naphthenic acids (NAs) predictions
 naph_predicted = naph_model.predict(decoder_predicted_test)
-##
-def phenol(input):
-    layer_1 = layers.Dense(units=2500, activation='elu')(input)
-    layer_11 = layers.Dense(units=1000, activation='elu')(layer_1)
-    layer_2 = layers.Dense(units=500, activation='elu')(layer_11)
-    layer_21 = layers.Dense(units=100, activation='elu')(layer_2)
-    layer_3 = layers.Dense(units=50, activation='elu')(layer_21)
-    layer_4 = layers.Dense(units=10, activation='elu')(layer_3)
-    output = layers.Dense(units=1, activation='elu')(layer_4)
-    return output
-inputs = layers.Input(shape=(np.shape(decoder_predicted_train)[1],))
-phenol_output = phenol(inputs)
-phenol_model = Model(inputs=inputs, outputs=phenol_output)
-phenol_model.compile(optimizer='adam', loss='mean_squared_error')
-deep_auto_history = phenol_model.fit(
-    x=decoder_predicted_train,
-    y=train_Phenol,
-    epochs=300,
-    batch_size=32,
-    shuffle=True,
-    validation_split=0.2
-)
-phenol_predicted = phenol_model.predict(decoder_predicted_test)
-# Evaluate the results for the test data
-##
+
+## Evaluation metrics for contaminant's quantification
 mae_naph = mae(test_Naph, naph_predicted)
 r2_naph = r2(test_Naph, naph_predicted)
-
 MSE_naph = np.square(np.subtract(test_Naph, naph_predicted)).mean()
 RMSE_naph = float("{:.3f}".format(np.sqrt(MSE_naph)))
-
-##
-mae_phenol = mae(test_Phenol, phenol_predicted)
-r2_phenol = r2(test_Phenol, phenol_predicted)
-MSE_phenol = np.square(np.subtract(test_Phenol, phenol_predicted)).mean()
-RMSE_phenol = float("{:.3f}".format(np.sqrt(MSE_phenol)))
-##
-mae_test = [mae_naph, mae_phenol]
-r2_test = [mae_naph, mae_phenol]
-y = 0
-##
-labels = ['Naphthenic','Phenol']
-concentration_predicted = [naph_predicted, phenol_predicted]
-concentration_predicted = np.array(concentration_predicted)
-concentration_predicted.shape = [2, test_quantity]
-concentration_predicted = concentration_predicted.transpose()
 
 real_datafrane = pd.DataFrame(test_concentration, columns=labels)
 predicted_dataframe = pd.DataFrame(concentration_predicted, columns=labels)
 
+## Plot predicted concentrations vs real concentrations
 plt.figure(figsize=(10,10))
-plt.title('7 inputs')
-plt.scatter(real_datafrane['Naphthenic'], predicted_dataframe['Naphthenic'], c='crimson', label="Naphthenic")
-plt.scatter(real_datafrane['Phenol'], predicted_dataframe['Phenol'], c='blue', label='Phenol')
+plt.title('1 dense layer', fontsize=20)
+plt.scatter(test_Naph, naph_predicted, c='crimson', label="Naphthenic")
 
 x = np.linspace(0, 100)
 plt.plot(x, x, c='black', linestyle='dotted')
-
-plt.xlabel('Measured NAs (mg/L) and Phenol (${\mu}$g/L)', fontsize=15)
-plt.ylabel('Predicted NAs (mg/L) and Phenol (${\mu}$g/L)', fontsize=15)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.xlabel('Measured NAs (mg/L) and Phenol (${\mu}$g/L)', fontsize=20)
+plt.ylabel('Predicted NAs (mg/L) and Phenol (${\mu}$g/L)', fontsize=20)
 plt.axis('square')
 plt.grid(linestyle='dotted', linewidth=1)
-plt.legend(fontsize=14)
+plt.legend(fontsize=20)
 plt.ylim(0, 100)
 plt.xlim(0, 100)
 plt.show()
-##
-y = 0
-
-#predicted_dataframe.to_csv('Lake_Concentration_RF_7.csv', index=False)
-predicted_dataframe.to_csv('Lake_Concentration_NN_7.csv', index=False)
-y = 0
-
